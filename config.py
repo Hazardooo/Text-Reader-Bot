0ca915d70564110514d6b6b5fe35166269fe700c
Pavel <hazdoo@outlook.com>
2022-12-28 04:37:24 +0300

    PIC-TEXT-TO-STR alpha 0.0.1
    The bot is able to recognize the text on the image.
    
    At this stage, the bot is able to qualitatively recognize only clearly distinguished text on the image.
    
    Language switch.
    
    There is a configuration file for more convenient use of the bot. It is created automatically if suddenly it is missing.
    
    Auto-creation of the user's image folder.  Checking the presence of the user's folder.
    
    Auto-delete user images.

---
+++ config.py
@@ -0,0 +1,38 @@
+from os import path, mkdir, remove, listdir
+
+
+class New_User():
+    def __init__(self, user_id: str) -> str:
+        self.user_id = str(user_id)
+        self.way = path.abspath(__name__).replace(__name__, "")+'photos/'
+        self.user_image = self.way+self.user_id
+
+    def catalog(self):
+        if not path.exists(self.way):
+            mkdir(self.way)
+        if not path.exists(self.user_image):
+            mkdir(self.user_image)
+            return self.user_image
+        return self.user_image
+
+    def clear_catalog(self):
+        for file in listdir(self.user_image):
+            remove(path.join(self.user_image, file))
+
+
+class Settings_config(New_User):
+    def __init__(self, user_id=None, token=True) -> str:
+        super().__init__(user_id)
+        self.token = token
+
+    def check_settings(self):
+        if not path.exists(self.way.replace("photos/", "config.txt")):
+            with open("config.txt", "w", encoding='UTF-8') as f:
+                f.write("Hi my friend! This is the bot settings!!!\n")
+                f.write("Please do not change this file because I decided not to complicate the code and made the bot setup positional on the lines of this file\n")
+                f.write("\nBOT TOKEN API: ")
+
+    def TOKEN_API(self):
+        with open("config.txt", "r", encoding='UTF-8') as f:
+            self.token = f.readlines()[3].split()[-1]
+            return self.token
---
+++ main.py
@@ -0,0 +1,43 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ContentType, Message
+from piceye import Img_to_str
+from config import Settings_config, New_User
+
+
+Settings_config().check_settings()
+try:
+    bot = Bot(Settings_config().TOKEN_API())
+except:
+    print("У вас нет токена!")
+    exit(input("press Enter..."))
+dp = Dispatcher(bot)
+Lang_Pointer = Img_to_str()
+Lang_Pointer.lang_point(True)
+
+
+@dp.message_handler(commands=["start"])
+async def encho(message: Message):
+    await message.answer(text="Приветсвую друг!", reply_markup=ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton("🇷🇺"), KeyboardButton("🇺🇸")))
+
+
+@dp.message_handler(content_types=ContentType.PHOTO)
+async def re_return(message: Message):
+    user = New_User(message.from_user.id)
+    await message.photo[-1].download(destination_file=user.catalog()+'/image.png')
+    await message.answer(text=Lang_Pointer.recognition_text(user.catalog()+'/image.png', Lang_Pointer.flag))
+    user.clear_catalog()
+
+
+@dp.message_handler()
+async def Lang(message: Message):
+    if message.text == '🇷🇺':
+        await message.answer(text="RU!")
+        Lang_Pointer.lang_point(True)
+
+    elif message.text == '🇺🇸':
+        await message.answer(text="USA!")
+        Lang_Pointer.lang_point(False)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
